6c579f768ada6b7b6de5ec3dc9cf683bb5df2e33
dylan <dylanpjx@gmail.com>
2022-06-22 21:21:43 +0800

    Update autorun.sh to xset after a delay

--- linux/scripts/autorun.sh
+++ linux/scripts/autorun.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 function run {
   if ! pgrep -f $1 ;
   then
@@ -15,10 +14,8 @@ run quodlibet
 run discord
 run alacritty
 
+sleep 3
 xset s off
 xset -dpms
 xset s noblank
 xset r rate 250 30
-xsetwacom set "HUION Huion Tablet stylus" MapToOutput HEAD-0
-
-nitrogen --restore &
