14a95ad2a19e82a191bec8e47a440ddff97fe9be
dylan <dylanpjx@gmail.com>
2022-10-25 14:41:36 +0800

    Update wbring to exclude current tag

--- linux/.config/herbstluftwm/scripts/wbring.sh
+++ linux/.config/herbstluftwm/scripts/wbring.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 hc() { ${herbstclient_command:-herbstclient} "$@" ;}
-currtag=$(hc attr tags.focus | grep "\sindex"  | tr -dc '0-9')
-id=$(wmctrl -l | rofi -dmenu -i -p "Bring" | awk '{ print $1 ; }')
+currtag=$(hc attr tags.focus.index)
+id=$(wmctrl -l | grep -v " $currtag "| rofi -dmenu -i -p "Bring" | awk '{ print $1 ; }')
 hc bring ${id}
