a354c6f8ed2b3626026959acd6cabf50c7dffdf3
dylan <dylanpjx@gmail.com>
2022-11-30 19:34:43 +0800

    Add ability to view screenshots

--- home/scripts/screenshot.sh
+++ home/scripts/screenshot.sh
@@ -3,3 +3,13 @@
 imgpath="$HOME/Pictures/$(date +%F-%T).png"
 maim -g $(slop) -u $imgpath
 xclip -selection clipboard -t image/png -i $imgpath
+
+viewImgAction() {
+  xdg-open $imgpath
+}
+action=$(DISPLAY=:0 dunstify -t 2000 -h string:x-dunst-stack-tag:ss "Screenshot saved" --action="viewImg, View")
+
+if [ "$action" = "viewImg" ]
+then
+  viewImgAction
+fi
