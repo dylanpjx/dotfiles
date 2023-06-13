6c6dcc963589a411feb5a98622bc0e030934c6e6
dylan <dylanpjx@gmail.com>
2023-06-13 16:33:57 +0800

    Update nvim rc

--- home/.config/nvim/lua/opts/mappings.lua
+++ home/.config/nvim/lua/opts/mappings.lua
@@ -1,6 +1,8 @@
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true }
 
 -- Editor
+vim.keymap.set('i', 'jk', '<Esc>', opts)
+
 vim.api.nvim_exec([[
   nnoremap <expr> j v:count ? 'j' : 'gj'
   nnoremap <expr> k v:count ? 'k' : 'gk'
@@ -29,7 +31,7 @@ vim.keymap.set('n', 'H', 'gT', opts)
 vim.keymap.set('n', 'L', 'gt', opts)
 
 vim.keymap.set('n', '<leader>cd', '<cmd>tcd %:p:h<bar>pwd<CR>', opts)
-vim.keymap.set('n', '-', '<cmd>EditVifm<CR>', opts)
+vim.keymap.set('n', '-', '<cmd>Ex<CR>', opts)
 
 vim.keymap.set('n', '<leader>ws', '<cmd>mksession! ws.vim <bar> <bar> <cmd>wqa <CR>', opts)
 vim.keymap.set('n', '<silent>gx', '<cmd>!xdg-open <c-r>=shellescape(expand("<cfile>"))<CR><CR>', opts)
@@ -45,8 +47,8 @@ vim.keymap.set('n', '<C-w>O',  '<C-w>_ | <C-w>|', opts) vim.keymap.set('n', '<C-
 vim.keymap.set('n', '\\', '<cmd>s/\\s\\+$//e<CR>', opts) -- remove trailing whitespace
 vim.keymap.set('v', '//', 'y/\\V<C-R>=escape(@","/\")<CR><CR>', opts)
 
-vim.keymap.set('n', '<leader>c', '<cmd>AsyncRun comp.sh % <CR>', { noremap=true })
-vim.keymap.set('n', '<leader>o', '<cmd>!opout.sh % <CR>', { noremap=true })
+vim.keymap.set('n', '<leader>c', '<cmd>AsyncRun comp.sh % <CR>', opts)
+vim.keymap.set('n', '<leader>o', '<cmd>!opout.sh % <CR>', opts)
 
 vim.keymap.set('n', 'yn', '<cmd>let @+ = expand("%") <bar> echo expand("%") "is yanked to clipboard"<CR>', opts)
 vim.keymap.set('n', 'yp', '<cmd>let @+ = expand("%:p") <bar> echo expand("%:p") "is yanked to clipboard"<CR>', opts)
