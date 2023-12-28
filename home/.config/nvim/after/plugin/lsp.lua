34af131eb5f6f7b20a50e61913add7251caaa1aa
dylanpjx <dylanpjx@gmail.com>
2023-12-28 19:51:13 +0800

    Update LSP

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -1,55 +1,68 @@
-local lsp = require('lsp-zero')
+require('mason').setup()
+require('mason-lspconfig').setup{
+  ensure_installed = {
+    'vimls', 'lua_ls', 'pyright', 'clangd', 'svlangserver',
+  }
+}
 
-lsp.preset({})
+local lspconfig = require('lspconfig')
+lspconfig.vimls.setup{}
+lspconfig.lua_ls.setup{}
+lspconfig.pyright.setup{}
+lspconfig.clangd.setup{}
+lspconfig.svlangserver.setup{}
 
-lsp.ensure_installed({
-  'vimls',
-  'lua_ls',
-  'pyright',
-  'clangd',
-  'svlangserver'
-})
-
--- CMP
-local cmp = require('cmp')
+local cmp     = require('cmp')
 local luasnip = require('luasnip')
 
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  -- go to next placeholder in the snippet
-  ['<C-n>'] = cmp.mapping(function(fallback)
-    if luasnip.jumpable(1) then
-      luasnip.jump(1)
-    else
-      fallback()
-    end
-  end, { 'i', 's' }),
-  -- go to previous placeholder in the snippet
-  ['<C-p>'] = cmp.mapping(function(fallback)
-    if luasnip.jumpable(-1) then
-      luasnip.jump(-1)
-    else
-      fallback()
-    end
-  end, { 'i', 's' }),
-  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  ['<CR>'] = cmp.mapping.confirm({ select = false })
-})
-
-lsp.setup_nvim_cmp({
-  select_behavior = 'insert',
-  preselect = 'none',
-  completion = {
-    completeopt = 'menu,menuone,noselect'
+cmp.setup({
+  preselect = 'true',
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
+    end,
   },
+
+  mapping = cmp.mapping.preset.insert({
+    -- go to next placeholder in the snippet
+    ['<C-n>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    -- go to previous placeholder in the snippet
+    ['<C-p>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ["<Tab>"]     = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<S-Tab>"]   = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<C-b>']     = cmp.mapping.scroll_docs(-4),
+    ['<C-f>']     = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>']     = cmp.mapping.abort(),
+    ['<CR>']      = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
   sources = {
     { name = 'nvim_lsp_signature_help' },
     { name = 'luasnip' },
-    { name = 'buffer' },
+    {
+      name = 'buffer',
+      option = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end
+      }
+    },
     { name = 'nvim_lsp' },
     { name = 'path' },
-  },
-  mapping = cmp_mappings,
+ },
 })
 
 cmp.setup.cmdline({ '/', '?' }, {
@@ -68,31 +81,36 @@ cmp.setup.cmdline(':', {
   })
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
--- LSP
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  local keymap = vim.keymap.set
-  local opts = { noremap = true }
-
-  -- Mappings
-  keymap('n', ';w', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-
-  buf_set_keymap('n', ';r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', ';a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', ';f', '<cmd>LspZeroFormat<CR>', opts)
-  buf_set_keymap('n', ';d', '<cmd>lua vim.diagnostic.disable() <CR>', opts)
-  buf_set_keymap('n', ';e', '<cmd>lua vim.diagnostic.enable() <CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-end)
-
-lsp.setup()
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
 
 vim.diagnostic.config({
   virtual_text = { severity = vim.diagnostic.severity.ERROR },
-  underline = { severity = vim.diagnostic.severity.ERROR }
+  underline    = { severity = vim.diagnostic.severity.ERROR }
 })
--- home/.config/nvim/lua/opts/lazy.lua
+++ home/.config/nvim/lua/opts/lazy.lua
@@ -25,29 +25,24 @@ require("lazy").setup({
     end
   },
 
-  -- LSP
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
+  -- LSP Support
+  'neovim/nvim-lspconfig' ,
+  'williamboman/mason.nvim' ,
+  'williamboman/mason-lspconfig.nvim' ,
 
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
-      { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+  -- Autocompletion
+  'hrsh7th/nvim-cmp' ,
+  'hrsh7th/cmp-buffer' ,
+  'hrsh7th/cmp-path' ,
+  'saadparwaiz1/cmp_luasnip' ,
+  'hrsh7th/cmp-nvim-lsp' ,
+  'hrsh7th/cmp-nvim-lua' ,
+  'hrsh7th/cmp-nvim-lsp-signature-help' ,
 
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
-    }
-  },
+  -- Snippets
+  'L3MON4D3/LuaSnip' ,
+  'rafamadriz/friendly-snippets' ,
+
   'hrsh7th/cmp-cmdline',
 
   -- Notes
