93d8837f01eec6847cefc0aa3cf9e5e2a077d93c
dylanpjx <dylanpjx@gmail.com>
2024-02-16 15:01:44 +0800

    Fix mouse_batt script to display 0% even if erroring

--- home/scripts/mouse_batt.py
+++ home/scripts/mouse_batt.py
@@ -3,7 +3,7 @@ from openrazer.client import DeviceManager
 
 device_manager = DeviceManager()
 
-if (device_manager.devices[0]):
+try:
     print("%3d%%" % (device_manager.devices[0].battery_level))
-else:
-    print(" NA")
+except:
+    print("  0%")
