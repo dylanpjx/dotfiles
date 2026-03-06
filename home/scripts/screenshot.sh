d8866447cf2b74d844b304f0f29362dce1d36a04
dylan <dylanpjx@gmail.com>
2026-03-06 11:24:03 +0800

    Fix imgpath

--- home/scripts/screenshot.sh
+++ home/scripts/screenshot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-imgpath="$HOME/Pictures/$(date +%F-%T).png"
+imgpath="$HOME/Screenshots/$(date +%F-%T).png"
 maim -g $(slop) -u $imgpath
 xclip -selection clipboard -t image/png -i $imgpath
 
